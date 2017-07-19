0fc788f50b3945dc5745a30f1f49a3d2d6445b11
zhouhao <zh.sqy@qq.com>
2017-07-19 16:45:39 +0800

    修改目录结构

--- hsweb-ui-plugins/hsweb-ui-ueditor/src/main/resources/static/plugins/ueditor/third-party/zeroclipboard/ZeroClipboard.js
+++ hsweb-ui-plugins/hsweb-ui-ueditor/src/main/resources/static/plugins/ueditor/third-party/zeroclipboard/ZeroClipboard.js
@@ -1242,15 +1242,15 @@
   };
   _globalConfig.hoverClass = "zeroclipboard-is-hover";
   _globalConfig.activeClass = "zeroclipboard-is-active";
-  if (typeof define === "function" && define.amd) {
-    define(function() {
-      return ZeroClipboard;
-    });
-  } else if (typeof module === "object" && module && typeof module.exports === "object" && module.exports) {
-    module.exports = ZeroClipboard;
-  } else {
+  // if (typeof define === "function" && define.amd) {
+  //   define(function() {
+  //     return ZeroClipboard;
+  //   });
+  // } else if (typeof module === "object" && module && typeof module.exports === "object" && module.exports) {
+  //   module.exports = ZeroClipboard;
+  // } else {
     window.ZeroClipboard = ZeroClipboard;
-  }
+  // }
 })(function() {
   return this;
 }());
