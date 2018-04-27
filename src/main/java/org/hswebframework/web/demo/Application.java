7c410242d148730dd2172905174a53b96c0592de
zhou-hao <i@hsweb.me>
2018-04-28 00:34:26 +0800

    jta

--- src/main/java/org/hswebframework/web/demo/Application.java
+++ src/main/java/org/hswebframework/web/demo/Application.java
@@ -2,8 +2,13 @@ package org.hswebframework.web.demo;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.hswebframework.utils.StringUtils;
+import org.hswebframework.web.AopUtils;
+import org.hswebframework.web.authorization.annotation.Authorize;
 import org.hswebframework.web.authorization.basic.configuration.EnableAopAuthorize;
 import org.hswebframework.web.authorization.listener.event.AuthorizingHandleBeforeEvent;
+import org.hswebframework.web.commons.entity.param.QueryParamEntity;
+import org.hswebframework.web.controller.QueryController;
+import org.hswebframework.web.controller.authorization.UserController;
 import org.hswebframework.web.dao.Dao;
 import org.hswebframework.web.loggin.aop.EnableAccessLogger;
 import org.mybatis.spring.annotation.MapperScan;
@@ -30,7 +35,7 @@ import java.io.UnsupportedEncodingException;
 public class Application implements ApplicationListener<AuthorizingHandleBeforeEvent> {
 
 
-    public static void main(String[] args) throws UnsupportedEncodingException {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class);
     }
 
